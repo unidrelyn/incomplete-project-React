78ef59ba5c6e5328431efb4fa9f6f93096e7b08c
unidrelyn <unidrelyn@gmail.com>
2020-06-11 22:42:20 +0200

    header

---
+++ src/app/components/Topbar/layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import logo from '../../../assets/images/logo-almundo.svg';
+
+function Layout() {
+	return (
+		<nav className={styles.navbar}>
+			<img className={styles.image} alt='imagen SVG' src={logo} />
+		</nav>
+	);
+}
+
+export default Layout;
